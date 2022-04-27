77bf822449b07a26907107f8a3138657ed711a56
loulimynu <64753817+loulimynu@users.noreply.github.com>
2022-04-27 11:41:35 -0300

    Update ROT13.java

--- ROT13.java
+++ ROT13.java
@@ -1,60 +1,4 @@
-package project.rot13;
-
-import java.util.Scanner;
-
-/**
- *
- * @author white
- */
-public class ROT13 {
-
-    public static void main(String[] args) {
-        //Ambinete teorico onde você instanciaria o 
-        //objeto crypt para realizar a criptografia 
-        //em algum campo.
-        
-        //variaveis de inicialização
-        Scanner in = new Scanner(System.in);//novo objeto de scan
-        String rotmsg;/*variavel de exemplo, sera usada apenas para
-        armazenar o valor da criptografia para podermos comparalas,
-        em um ambinete real seu funcao seria aplicada a variavel msg.*/
-        
-        //menu.
-        System.out.println("1- Encryption\n2- Decryption\nChose(1,2)");// "\n" quebra o output para a outra linha.
-        int chose = in.nextInt();
-        
-        //criando objeto crypto, ele vai ser ultilizado para chamar o metodo em sua chace.
-        CryptRot13 crypt = new CryptRot13();
-        
-        //operador logico especifico para menus.
-        switch (chose) {
-            case 1 -> {
-                System.out.println("---Encryption---\nOnly alphabetics characters\nEnter menssage: ");
-                String msg = in.next();
-                rotmsg = crypt.cipherEncryption(msg);//msg = crypt.cipherEncryption(msg)
-                //outpute somente para comparação
-                System.out.println("\nMensagem antiga: " + msg + "\nNova mensagem: " + rotmsg);
-                break;
-            }
-            case 2 -> {
-                System.out.println("---Decryption---");
-                String msg = in.next();
-                //uma vez que a cifra move um caractere seu equivalente 13 caracteres a frente
-                //aplicar a cifra em uma String ja criptografada a descriptografa
-                rotmsg = crypt.cipherEncryption(msg);
-                System.out.println("\nMensagem antiga: " + msg + "\nNova mensagem: " + rotmsg);
-                break;
-            }
-            //default é para onde vc sera encaminhado caso não se encaixe em nenhum dos operadores logicos a cima.
-            default -> {
-                    System.out.println("---Invalide---");
-                    break;
-            }
-        }
-    }
-
-    //Classe proprosta que seria estanciada
-    private static class CryptRot13 {
+private static class CryptRot13 {
 
         public String cipherEncryption(String str) {
 		StringBuilder val = new StringBuilder();
@@ -79,5 +23,3 @@ public class ROT13 {
 		return val.toString();
                 //retorna caracteres encontrados como String.
 	}
-    }
-}
